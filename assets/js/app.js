36f6f7a0162334dcf47742f5072ffceb3ef7e42e
Shubham Ashish <shubhamashish8@gmail.com>
2021-12-26 19:47:36 +0530

    Update algorithm

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,24 @@
 var c1 = document.getElementById("choice1");
 var c2 = document.getElementById("choice2");
+var result = document.getElementById("result");
+var wrapper = $(".input_fields_wrap");
 
-const max = 100
-const min = 1
-var answer = Math.floor(Math.random() * 100) + 1;
+// Random Number Generator
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
+// Main Function
 function run() {
-    console.log("hey");
-    console.log(answer);
+  var choice = getRandomInt(2);
+  console.log(choice);
+  if (c1.value == "" || c2.value == "") {
+    alert("Enter Choices");
+  } else {
+    if (choice == 0) {
+      result.textContent = "You have to go with " + c1.value;
+    } else if (choice == 1) {
+      result.textContent = "You have to go with " + c2.value;
+    }
+  }
 }
