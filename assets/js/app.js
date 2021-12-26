cc44a9d151a5a146d33f7dff248595969edd504f
Shubham Ashish <shubhamashish8@gmail.com>
2021-12-26 18:52:22 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+var c1 = document.getElementById("choice1");
+var c2 = document.getElementById("choice2");
+
+const max = 100
+const min = 1
+var answer = Math.floor(Math.random() * 100) + 1;
+
+function run() {
+    console.log("hey");
+    console.log(answer);
+}
